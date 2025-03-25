bcbe36489e21ce93cbf88a5092ccb7c473c5690b
null128 <2247019143@qq.com>
2025-03-25 08:58:56 +0800

    update

--- assets/script/SpeedController.ts
+++ assets/script/SpeedController.ts
@@ -17,16 +17,18 @@ export class SpeedControl extends Component {
 
     update(dt: number) {
         // 计算位移差
-        const delta = this.controllerModel.position.subtract(this._lastPos);
+        const currentPos=this.controllerModel.position.clone();
+        const delta = currentPos.subtract(this._lastPos);
         console.log("controller position:",this.controllerModel.position.toString());
         console.log("controller delta: ",delta.length().toString());
 
         // 更新记录点
-        this._lastPos.set(this.controllerModel.position.clone());
+        this._lastPos=this.controllerModel.position.clone();
         
         this.character.translate(new Vec3(0,0,-delta.length()));
         console.log("character position:",this.character.position.toString());
 
+        //this.controllerModel.translate(new Vec3(0,0,-5*dt));
     }
 }
 
